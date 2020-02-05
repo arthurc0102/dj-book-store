af7d317494fc2b4fc595b6fe1ac2777ff7a4f8bf
Arthur Chang <arthurc0102@gmail.com>
2020-02-05 15:11:20 +0800

    Only admin can write

--- books/views.py
+++ books/views.py
@@ -1,7 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from django_filters import rest_framework as filters
+
+from utils.permissions import IsAdminUserOrReadOnly
 
 from .models import Book
 from .serializers import BookSerializer
@@ -12,4 +13,4 @@ class BookViewSet(ModelViewSet):
     serializer_class = BookSerializer
     filter_backends = (filters.DjangoFilterBackend,)
     filterset_fields = ('is_online',)
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAdminUserOrReadOnly]
